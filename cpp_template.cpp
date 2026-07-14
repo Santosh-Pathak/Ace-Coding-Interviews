59bae50e24bcb1ef2d4f46328780a0519b6eac20
Santosh Pathak <santosh.pathak@tedekstra.com>
2026-07-14 22:45:25 +0530

    Add initial implementation of C++ template for competitive programming

---
+++ cpp_template.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define endl '\n'
+
+// Output shortcuts
+#define out(x) cout << (x) << endl;
+#define print(arr) for (auto &x : (arr)) cout << x << ' '; cout << '\n'
+
+// DP shortcuts
+#define dp1(type, name, n, val) vector<type> name((n), (val))
+#define dp2(type, name, n, m, val) vector<vector<type>> name((n), vector<type>((m), (val)))
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+    }
+
+    return 0;
+}
